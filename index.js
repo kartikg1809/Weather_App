25a144ee69d31318fb640774dc1222efac3ad4f6
kartikg1809 <kartikg1809@gmail.com>
2023-12-30 04:00:29 +0530

    Designed Home page and header

---
+++ index.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port=3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'))
+
+app.get('/', (req, res) =>{
+    res.render('index.ejs');
+});
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
+})
