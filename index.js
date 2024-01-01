9218ba81a00bb0fb546c815a3222eeb96c79f5c6
kartikg1809 <kartikg1809@gmail.com>
2024-01-01 15:02:47 +0530

    Added api for five days and current day

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port=3000;
-let apikey="ONzfMcqyrt1vUZ8BI1ipgM1Bxkpwr8Eb";
+let apikey="vA5h7FdJWut9IMwHjOwoEb6e8G3zh2Sw";
 let URL="http://dataservice.accuweather.com/locations/v1";
 let URL2="https://dataservice.accuweather.com/currentconditions/v1/"
 let weather;
@@ -21,8 +21,9 @@ app.get('/', async (req, res) =>{
         const result = await axios.get("http://dataservice.accuweather.com/currentconditions/v1/topcities/50",config);
         console.log(result);
         res.render('index.ejs',{content:result.data});
-    }catch(e){
-
+    }catch (error) {
+        console.error("Error:", error);
+        res.render("error.ejs");
     }
 });
 
@@ -31,6 +32,9 @@ app.get('/oneDay', (req, res) =>{
 });
 app.get('/current', (req, res) =>{
     res.render('current.ejs',{data:weather});
+});
+app.get('/oneday', (req, res) =>{
+    res.render('oneday.ejs',{data:weather});
 });
 app.post("/currentforecast", async (req, res) => {
     const searchId = req.body.id;
@@ -54,9 +58,65 @@ app.post("/currentforecast", async (req, res) => {
       console.log(result2.data);
       res.render("current.ejs", { weather: result2.data,city:searchId.charAt(0).toUpperCase() + searchId.slice(1).toLowerCase()});
     } catch (error) {
-      weather="Something went wrong";
+        console.error("Error:", error);
+        res.render("error.ejs", { error: "Something went wrong" });
     }
   });
+  app.post("/onedayforecast", async (req, res) => {
+    const searchId = req.body.id;
+    try {
+        let config={
+            params: {
+                apikey:apikey,
+                q: searchId,
+            },
+        };
+      const result = await axios.get(URL + "/cities/search", config);
+      let temp=result.data;
+      let key=temp[0].Key;
+      let config2={
+        params: {
+            apikey:apikey,
+            details:true,
+        },
+      }
+      const result2 = await axios.get("http://dataservice.accuweather.com/forecasts/v1/daily/1day/" +key, config2);
+      console.log(result2.data);
+      res.render("oneDay.ejs", { weather: result2.data});
+    } catch (error) {
+        console.error("Error:", error);
+        res.render("error.ejs", { error: "Something went wrong" });
+    }
+  });
+  app.post("/fivedayforecast", async (req, res) => {
+    const searchId = req.body.id;
+    try {
+        let config={
+            params: {
+                apikey:apikey,
+                q: searchId,
+            },
+        };
+      const result = await axios.get(URL + "/cities/search", config);
+      let temp=result.data;
+      let key=temp[0].Key;
+      let config2={
+        params: {
+            apikey:apikey,
+            details:true,
+        },
+      }
+      const result2 = await axios.get("http://dataservice.accuweather.com/forecasts/v1/daily/5day/" +key, config2);
+      console.log(result2.data);
+      res.render("fiveday.ejs", { weather: result2.data});
+    } catch (error) {
+        console.error("Error:", error);
+        res.render("error.ejs", { error: "Something went wrong" });
+    }
+  });
+  app.get('/fiveday', (req, res) =>{
+    res.render('fiveday.ejs',{data:weather});
+});
 
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
